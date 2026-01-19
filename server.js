128ffaa85dfe754448d279f29ebc9f52dc47e1e0
Aditya Restu H <adityarestuhudayana@gmail.com>
2026-01-19 18:52:02 +0700

    change max json size

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 /* =================== DATABASE =================== */
 let isConnected = false;
